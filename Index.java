9153c5c220f4b726c96a2b5db01d2a12e33135f6
Timon <timon-a@outlook.at>
2025-02-07 10:25:06 +0100

    setup

---
+++ Index.java
@@ -0,0 +1,20 @@
+//import level1.level1;
+//import level2.level2;
+//import level3.level3;
+//import level4.level4;
+
+public class Index {
+    public static void main(String[] args) {
+        //level1 l1 = new level1();
+        //l1.run();
+
+        //level2 l2 = new level2();
+        //l2.run();
+
+        //level3 l3 = new level3();
+        //l3.run();
+
+        //level4 l4 = new level4();
+        //l4.run();
+    }
+}
